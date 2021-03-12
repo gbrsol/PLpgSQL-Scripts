d18639580f61d71490c795828eb43e2eef4d032c
Gabriel <gabriel.nverchai@gmail.com>
2021-03-12 10:42:48 -0300

    Drop scripts

---
+++ drop_empty_cols.sql
@@ -0,0 +1,21 @@
+﻿CREATE OR REPLACE FUNCTION deleteEmptyCols() 
+RETURNS VOID AS $$
+DECLARE
+	tabs CURSOR FOR SELECT tablename, attname 
+		FROM pg_stats
+		WHERE most_common_vals IS NULL
+		AND schemaname = 'public'
+		AND most_common_freqs IS NULL
+		AND histogram_bounds IS NULL
+		AND correlation IS NULL
+		AND null_frac = 1;
+BEGIN
+	FOR reg IN tabs
+	LOOP
+		EXECUTE FORMAT('alter table %s drop column %s',reg.tablename,reg.attname);
+	END LOOP;
+	
+END;
+$$ LANGUAGE 'plpgsql';
+
+SELECT deleteCols();
---
+++ drop_empty_tables.sql
@@ -0,0 +1,15 @@
+﻿CREATE OR REPLACE FUNCTION dropEmptyTables(name TEXT)
+  RETURNS void AS
+$BODY$
+DECLARE statement TEXT;
+BEGIN
+statement := 'DROP TABLE ' || name;
+EXECUTE statement;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE SECURITY DEFINER
+  COST 100;
+
+SELECT dropEmptyTables(relname)
+FROM pg_stat_user_tables
+WHERE n_live_tup = 0;
